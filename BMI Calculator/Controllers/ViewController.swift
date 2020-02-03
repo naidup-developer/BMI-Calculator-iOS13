f9a12bccff55007d556e0afca231578fa06ad27c
Naidu <panaidu541@gmail.com>
2020-02-03 15:41:51 +0530

    calculation done..

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onHeightSliderChanged(_ sender: UISlider) {
+        
+        //to display only two decimals
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func onWeightSliderChanged(_ sender: UISlider) {
+        
+        //display without decimal
+        print(Int(sender.value))
+        
+        weightLabel.text = "\(String(format: "%.f", sender.value))kg"
+    }
+    
+    @IBAction func onClickCalculate(_ sender: UIButton) {
+        
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        //BMI = weight/(height * height)
+        
+        let bmi = weight/pow(height, 2.0)
+        
+        print("BMI = \(bmi)")
+    }
 }
 
